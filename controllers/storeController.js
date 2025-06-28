607bc84ef594798d88ea75af8f77d0230897acd4
Arijit166 <arijit.sarkar7156@gmail.com>
2025-06-28 19:12:12 +0530

    Update edit and delete buttons

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,5 @@
+const Favourite = require("../models/favourite");
 const Home = require("../models/home");
-const Favourite=require("../models/favourite")
 
 exports.getIndex = (req, res, next) => {
   Home.fetchAll((registeredHomes) =>
@@ -29,40 +29,50 @@ exports.getBookings = (req, res, next) => {
 };
 
 exports.getFavouriteList = (req, res, next) => {
-  Favourite.getFavourites(favourites =>{
-    Home.fetchAll((registeredHomes) =>{
-    const favouriteHomes=registeredHomes.filter(home => favourites.includes(home.id))
-    res.render("store/favourite-list", {
-      favouriteHomes: favouriteHomes,
-      pageTitle: "My Favourites",
-      currentPage: "favourites",
-    })
-    })
+  Favourite.getFavourites(favourites => {
+    Home.fetchAll((registeredHomes) => {
+      const favouriteHomes = registeredHomes.filter(home => favourites.includes(home.id));
+      res.render("store/favourite-list", {
+        favouriteHomes: favouriteHomes,
+        pageTitle: "My Favourites",
+        currentPage: "favourites",
+      })
+    });
+  })
+
+};
+
+exports.postAddToFavourite = (req, res, next) => {
+  Favourite.addToFavourite(req.body.id, error => {
+    if (error) {
+      console.log("Error while marking favourite: ", error);
+    }
+    res.redirect("/favourites");
   })
 }
 
-exports.postAddToFavourites=(req,res,next)=>{
-  console.log("Came to Add to favourites",req.body)
-  Favourite.addToFavourites(req.body.id, error =>{
-    if(error){
-      console.log("Error while marking favourite")
+exports.postRemoveFromFavourite = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Favourite.deleteById(homeId, error => {
+    if (error) {
+      console.log('Error while removing from Favourite', error);
     }
-    res.redirect("/favourites")
+    res.redirect("/favourites");
   })
 }
 
 exports.getHomeDetails = (req, res, next) => {
-  const homeId=req.params.homeId;
-  Home.findbyId(homeId,home=>{
-    if (!home){
-      console.log("Home not found")
-      res.redirect("/homes")
-    }else{
-    res.render("store/home-detail", {
-      home: home,
-      pageTitle: "Home Detail",
-      currentPage: "home",
+  const homeId = req.params.homeId;
+  Home.findById(homeId, home => {
+    if (!home) {
+      console.log("Home not found");
+      res.redirect("/homes");
+    } else {
+      res.render("store/home-detail", {
+        home: home,
+        pageTitle: "Home Detail",
+        currentPage: "Home",
+      });
+    }
   })
-   }
-   })
-  }
+};
