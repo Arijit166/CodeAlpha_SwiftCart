bd19ac73c1cf5c78e69a6de361904b981f51d6d7
Arijit166 <arijit.sarkar7156@gmail.com>
2025-07-04 01:14:50 +0530

    Upload cookies and sessions

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+exports.getLogin = (req, res, next) => {
+  res.render("auth/login", {
+    pageTitle: "Login",
+    currentPage: "Login",
+    isLoggedIn: false
+  })
+};
+exports.postLogin=(req,res,next)=>{
+  console.log(req.body);
+  req.session.isLoggedIn=true;
+  res.redirect("/")
+}
+exports.postLogout=(req,res,next)=>{
+  req.session.destroy(()=>{
+    res.redirect("/login")
+  })
+}
