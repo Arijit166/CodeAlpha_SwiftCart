3f03eccde774a8eb9509005eac701c22b97348c4
Arijit166 <arijit.sarkar7156@gmail.com>
2025-07-10 22:47:58 +0530

    Update bookings part

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,17 +1,38 @@
 const Home = require("../models/home");
 const User = require("../models/user");
 
-exports.getIndex = (req, res, next) => {
+
+exports.getIndex = async (req, res, next) => {
   console.log("Session Value: ", req.session);
-  Home.find().then((registeredHomes) => {
+
+  try {
+    const registeredHomes = await Home.find();
+    let bookedHomeIds = [];
+
+    if (req.session.user) {
+      const user = await User.findById(req.session.user._id);
+      if (user.bookings && user.bookings.length > 0) {
+        bookedHomeIds = user.bookings.map(id => id.toString());
+      }
+    }
+
     res.render("store/index", {
       registeredHomes: registeredHomes,
       pageTitle: "homezy Home",
       currentPage: "index",
-      isLoggedIn: req.isLoggedIn, 
+      isLoggedIn: req.isLoggedIn,
+      user: req.session.user,
+      bookedHomeIds: bookedHomeIds, // ✅ passed to EJS
+    });
+
+  } catch (err) {
+    console.error("Error loading index:", err);
+    res.status(500).render("error/500", {
+      pageTitle: "Error",
+      isLoggedIn: req.isLoggedIn,
       user: req.session.user,
     });
-  });
+  }
 };
 
 exports.getHomes = (req, res, next) => {
@@ -26,14 +47,62 @@ exports.getHomes = (req, res, next) => {
   });
 };
 
-exports.getBookings = (req, res, next) => {
+exports.getBookings = async (req, res, next) => {
+  const userId = req.session.user._id;
+  const user = await User.findById(userId).populate('bookings');
   res.render("store/bookings", {
+    bookingHomes: user.bookings,
     pageTitle: "My Bookings",
     currentPage: "bookings",
     isLoggedIn: req.isLoggedIn, 
     user: req.session.user,
   });
 };
+
+exports.postBookings = async (req, res, next) => {
+  const homeId = req.body.id;
+  const userId = req.session.user._id;
+
+  const user = await User.findById(userId); // 🚫 no populate here
+
+  // Don't create the field if it doesn't exist and nothing needs to be saved
+  if (user.bookings && user.bookings.some(b => b.toString() === homeId)) {
+    return res.redirect("/bookings");
+  }
+
+  if (user.bookings && user.bookings.length > 0) {
+    return res.render("store/bookings", {
+      bookingHomes: await User.findById(userId).populate('bookings').then(u => u.bookings),
+      pageTitle: "My Bookings",
+      currentPage: "bookings",
+      isLoggedIn: req.isLoggedIn,
+      user: req.session.user,
+      errorMessage: "❗ You can book  only one home at a time.",
+    });
+  }
+
+  // Only now assign and save
+  if (!user.bookings) {
+    user.bookings = [homeId]; // ✅ creates the field only if it didn't exist
+  } else {
+    user.bookings.push(homeId);
+  }
+
+  await user.save();
+  res.redirect("/bookings");
+};
+
+exports.postCancelBookings = async (req, res, next) => {
+  const homeId = req.params.homeId;
+  const userId = req.session.user._id;
+  const user = await User.findById(userId);
+  if (user.bookings.includes(homeId)) {
+    user.bookings = user.bookings.filter(book => book != homeId);
+    await user.save();
+  }
+  res.redirect("/");
+};
+
 
 exports.getFavouriteList = async (req, res, next) => {
   const userId = req.session.user._id;
--- routes/storeRouter.js
+++ routes/storeRouter.js
@@ -8,8 +8,9 @@ const storeController = require("../controllers/storeController");
 storeRouter.get("/", storeController.getIndex);
 storeRouter.get("/homes", storeController.getHomes);
 storeRouter.get("/bookings", storeController.getBookings);
+storeRouter.post("/bookings", storeController.postBookings);
 storeRouter.get("/favourites", storeController.getFavouriteList);
-
+storeRouter.post("/bookings/delete/:homeId", storeController.postCancelBookings);
 storeRouter.get("/homes/:homeId", storeController.getHomeDetails);
 storeRouter.post("/favourites", storeController.postAddToFavourite);
 storeRouter.post("/favourites/delete/:homeId", storeController.postRemoveFromFavourite);
